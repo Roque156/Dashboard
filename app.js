fb8b45b9fc5de9b52cd35d7c143aa63fca2d01a0
Rocaleta15lehsiM@outlook.com <Rocaleta15lehsiM@outlook.com>
2022-11-24 23:48:58 -0600

    Se resolvio problema de verificar estatus, falta resolver bug de covertir los datos a json antes de aplicar then a la promesa

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
 //Declaracion de variables
-const jumbotronUI=document.querySelector('.jumbotron');
+const menuUI=document.querySelector('.menu');
+const resultado = '';
 
-//document.addEventListener('DOMContentLoaded',getimg);
+document.addEventListener('DOMContentLoaded',getimg);
 
-/*async function getimg (){
 
+async function getimg (){
+    
     let randomNumber = Math.ceil(Math.random()*1000);
     const respuesta = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomNumber}`);
-    
-    if(respuesta.status === 200){
-    
-        const resultado = await respuesta.json();
+    const resultado = await respuesta.json();
 
-        console.log(resultado.primaryImage);
-    
-        if(resultado.primaryImage === ""){
+    respuesta.then (response=> {
+
+        let estado = response.status;
+        console.log (estado);
+        if(estado === 200 ){
+            console.log('exitoso');
+        
+        }else{
+            console.log('falla');
             getimg();
         }
+   
+       })
+   
+       .catch (error=> {
+   
+        console.log ("Error: " + error);
+   
+      });
 
-        jumbotronUI.innerHTML=`
-    
-        <img width="200px" src='${resultado.primaryImage}'>
-    
-        `
-    }
-    else {
-        getimg();
-    }
-    
-}*/
 
+    /*.then (response=> {
+
+     console.log (response.status);
+
+    })
+
+    .catch (error=> {
+
+     console.log ("Error: " + error);
+
+   });*/
+
+            console.log(randomNumber);
+
+            //console.log(resultado.primaryImage);
+    
+            /*resultado.primaryImage === ""
+
+            menuUI.innerHTML= menuUI.innerHTML+`
+    
+            <img width="200px" src='${resultado.primaryImage}'>
+    
+        //`*/
+}
