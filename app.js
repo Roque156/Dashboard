3042d907c1a9e26db3589b8fcc57b412131976fb
Rocaleta15lehsiM@outlook.com <Rocaleta15lehsiM@outlook.com>
2022-11-26 01:10:26 -0600

    Se repite misma logica para la siguiente estructura de la pagina que son temporales

--- app.js
+++ app.js
@@ -1,45 +1,34 @@
 //Declaracion de variables
-const menuUI=document.querySelector('.menu');
+const menuUI = document.querySelector('.menu');
+const temporalUI = document.querySelector('.temporales');
 let c=0;
+let t=0;
 
 document.addEventListener('DOMContentLoaded',getimg);
+document.addEventListener('DOMContentLoaded',getTemp);
 
 
 async function getimg (){
     
     let randomNumber = Math.ceil(Math.random()*1000);
     const respuesta = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomNumber}`)
-    //const resultado = await respuesta.json();
 
     .then (response=> {
 
         let estado = response.status;
-        //let datos = response.json();
-
-        //console.log (estado);
-        //console.log (response.json());
-        /*const resultado = response.json()
-        .then(response=>{
-            console.log(resultado);
-        })*/
-        /*console.log(*/response.json()/*)*/
+        response.json()
         .then(data=>{   
-            /*console.log(data);
-            console.log(estado);*/
+            
             if(estado == 200 && !data.primaryImage == "" || !data.primaryImage == undefined ){
-                /*console.log(data.primaryImage);
-                console.log(estado);*/
+        
                 if(c<6){
                     c++;
-                    console.log(data.primaryImage);
                     menuUI.innerHTML +=`
                     <img width="200px" src='${data.primaryImage}'>             
                     `
                     getimg();
                 }
             }else{
-                /*console.log(data.primaryImage + " " + 'no existe');
-                console.log(estado);*/
                 getimg();
             }
         });
@@ -51,29 +40,40 @@ async function getimg (){
         console.log ("Error: " + error);
    
       });
-
-
-    /*.then (response=> {
-
-     console.log (response.status);
-
-    })
-
-    .catch (error=> {
-
-     console.log ("Error: " + error);
-
-   });*/
-
-            //console.log(randomNumber);
-
-            //console.log(resultado.primaryImage);
     
-            /*resultado.primaryImage === ""
+}
 
-            menuUI.innerHTML= menuUI.innerHTML+`
+async function getTemp (){
     
-            <img width="200px" src='${resultado.primaryImage}'>
+    let randomNumber = Math.ceil(Math.random()*1000);
+    const respuesta = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomNumber}`)
+
+    .then (response=> {
+
+        let estado = response.status;
+        response.json()
+        .then(data=>{   
+            
+            if(estado == 200 && !data.primaryImage == "" || !data.primaryImage == undefined ){
+        
+                if(t<3){
+                    t++;
+                    temporalUI.innerHTML +=`
+                    <img width="200px" src='${data.primaryImage}'>             
+                    `
+                    getTemp();
+                }
+            }else{
+                getTemp();
+            }
+        });
+   
+       })
+   
+       .catch (error=> {
+   
+        console.log ("Error: " + error);
+   
+      });
     
-        //`*/
 }
